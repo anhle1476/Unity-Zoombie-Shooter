c8d80930d9f4809d0dfa713c8c7872b617fdb23f
anh.le <anhle1476@gmail.com>
2022-07-11 17:13:16 +0700

    collect ammo supply

---
+++ Assets/Script/Arsenal/AmmoSupply.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Script.Arsenal
+{
+    public class AmmoSupply : MonoBehaviour
+    {
+        [SerializeField]
+        private AmmoType ammoType = AmmoType.Riffle;
+        [SerializeField]
+        [Range(1, 100)]
+        private int amount = 10;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") == false)
+                return;
+
+            AmmoBag ammoBag = other.GetComponentInChildren<AmmoBag>();
+
+            if (ammoBag)
+            {
+                ammoBag.AmmoQuantity.CollectAmmo(ammoType, amount);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
